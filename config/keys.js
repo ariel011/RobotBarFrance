ecde38f98bd65f2bf821726e2302fc2df83f18b6
ariel011 <54809203+ariel011@users.noreply.github.com>
2019-12-14 21:17:44 +0100

    Production database added

--- config/keys.js
+++ config/keys.js
@@ -3,3 +3,15 @@ module.exports = {
     mongoURI: "mongodb://localhost:27017/meven_auth",
     secret: 'yoursecret'
 };
+
+if(process.env.NODE_ENV == 'production'){
+    module.exports = {
+        mongoURI: "mongodb+srv://a101496:Arsenal11!@cluster0-qfmm0.mongodb.net/test?retryWrites=true&w=majority",
+        secret: 'yoursecret'
+    };
+ } else {
+     module.exports = {
+         mongoURI: "",
+         secret: 'yoursecret'
+     };
+ } 
